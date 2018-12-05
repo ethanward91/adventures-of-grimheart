aeb9f0a2c5ce5d84a55ac8c3ceecbdc2e64cfc89
ethanward91 <graymaul@gmail.coms>
2018-12-04 19:59:16 -0500

    Initial Commit

---
+++ AdventuresOfGrimheart.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AdventuresOfGrimheart.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ AdventuresOfGrimheart.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AdventuresOfGrimheart.Models
+{
+    public class Class1
+    {
+    }
+}
